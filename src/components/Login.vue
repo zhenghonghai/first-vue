7e31fdb64c1ed002b14c20f339d7e5e70d618892
zhenghonghai <1092548413@qq.com>
2019-04-15 22:01:12 +0800

    我的第一个前端

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="login">
+        <h1>我是第一个项目测试</h1>
+        <div v-for="user in users"
+                :key="user.id">{{user.username}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return{
+            users: []
+        }
+    },
+    created(){
+    //   this.$http.get("https://jsonplaceholder.typicode.com/posts")
+    this.$http.post('/api/login')
+      .then(function(data){
+        this.users = data.body
+        console.log(this.users)
+      })
+  },
+}
+
+</script>
+
+<style>
+
+</style>
